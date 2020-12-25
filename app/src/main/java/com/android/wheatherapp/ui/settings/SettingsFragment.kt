eeb0b9daa6c12ab29712a78f61a9f5563747cc75
abhishek.s <asarnaik@softcell.com>
2020-12-25 19:43:02 +0530

    initial commit with base structure

---
+++ app/src/main/java/com/android/wheatherapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,19 @@
+package com.android.wheatherapp.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.wheatherapp.R
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
