84a6dc4644480874246421ad48d7d5e0c39e7924
abhishek.s <asarnaik@softcell.com>
2020-12-26 09:15:44 +0530

    api callbacks added

---
+++ app/src/main/java/com/android/wheatherapp/utils/Resource.kt
@@ -0,0 +1,24 @@
+package com.android.wheatherapp.utils
+
+/**
+ * Created by Abhishek.s on 26,December,2020
+ */
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/android/wheatherapp/utils/Status.kt
@@ -0,0 +1,10 @@
+package com.android.wheatherapp.utils
+
+/**
+ * Created by Abhishek.s on 26,December,2020
+ */
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
