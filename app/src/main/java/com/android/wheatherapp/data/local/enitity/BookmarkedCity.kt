c44c1559175cf1fd3ac5137e4e3e11fda02770ed
abhishek.s <asarnaik@softcell.com>
2020-12-27 11:22:30 +0530

    api key added to certificates

--- app/src/main/java/com/android/wheatherapp/data/local/enitity/BookmarkedCity.kt
+++ app/src/main/java/com/android/wheatherapp/data/local/enitity/BookmarkedCity.kt
@@ -3,7 +3,6 @@ package com.android.wheatherapp.data.local.enitity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.android.wheatherapp.data.model.WeatherMeta
 
 /**
  * Created by Abhishek.s on 26,December,2020
@@ -15,8 +14,8 @@ data class BookmarkedCity(
         @ColumnInfo(name = "name") var name: String?,
         @ColumnInfo(name = "lat") var lat: Double?,
         @ColumnInfo(name = "lon") var lon: Double?,
-        @ColumnInfo(name= "dt") var date: Long,
-        @ColumnInfo(name= "temp") var temp: Double?,
-        @ColumnInfo(name= "description") var description: String?,
-        @ColumnInfo(name="weather") var weather: String?
+        @ColumnInfo(name = "dt") var date: Long,
+        @ColumnInfo(name = "temp") var temp: Double?,
+        @ColumnInfo(name = "description") var description: String?,
+        @ColumnInfo(name = "weather") var weather: String?
 )
