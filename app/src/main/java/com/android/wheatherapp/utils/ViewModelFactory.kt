7ef3cc89b0ebb333dacdbe582e7ca2291e2ba7ce
abhishek.s <asarnaik@softcell.com>
2020-12-28 10:24:40 +0530

    Cities being bookmarked for local storage

--- app/src/main/java/com/android/wheatherapp/utils/ViewModelFactory.kt
+++ app/src/main/java/com/android/wheatherapp/utils/ViewModelFactory.kt
@@ -3,21 +3,18 @@ package com.android.wheatherapp.utils
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.android.wheatherapp.data.api.ApiHelper
+import com.android.wheatherapp.data.local.DatabaseHelper
 import com.android.wheatherapp.ui.city.CityViewModel
 import com.android.wheatherapp.ui.home.HomeViewModel
 
-class ViewModelFactory(private val apiHelper: ApiHelper) :
+class ViewModelFactory(private val apiHelper: ApiHelper, private val databaseHelper: DatabaseHelper) :
     ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
 
-        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(apiHelper) as T
-        }
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) return HomeViewModel(apiHelper, databaseHelper) as T
 
-        if (modelClass.isAssignableFrom(CityViewModel::class.java)) {
-            return CityViewModel(apiHelper) as T
-        }
+        if (modelClass.isAssignableFrom(CityViewModel::class.java)) return CityViewModel(apiHelper) as T
 
         throw IllegalArgumentException("Unknown class name")
     }
